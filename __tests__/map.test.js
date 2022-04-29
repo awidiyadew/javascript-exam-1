227b0813ae81fd8d04e72ff6884fbb0efc60986c
awidiyadew <awidiya.dewa@gmail.com>
2022-04-29 15:45:26 +0700

    (exam): create map test

--- __tests__/map.test.js
+++ __tests__/map.test.js
@@ -1,5 +1,36 @@
 import { addSerialNumber, halfNumbers, spliceNames } from "../src/map";
 
 describe("array map test", () => {
-  // Please add test cases here
+    describe('#halfNumbers', () => {
+        test('should return [2, 4] when numbers is [4, 8]', () => {
+            const numbers = [4, 8];
+            const expectedResult = [2, 4];
+
+            const actualResult = halfNumbers(numbers);
+
+            expect(actualResult).toEqual(expectedResult);
+        });
+    });
+
+    describe('#spliceNames', () => {
+        test('should return [hello john] when names is [john]', () => {
+            const names = ['john'];
+            const expectedResult = ['Hello john'];
+
+            const actualResult = spliceNames(names);
+
+            expect(actualResult).toEqual(expectedResult);
+        });
+    });
+
+    describe('#addSerialNumber', () => {
+        test('should return [1. orange, 2. apple] when fruits is [orange, apple]', () => {
+            const fruits = ['orange', 'apple'];
+            const expectedResult = ['1. orange', '2. apple'];
+
+            const actualResult = addSerialNumber(fruits);
+
+            expect(actualResult).toEqual(expectedResult);
+        });
+    });
 });
